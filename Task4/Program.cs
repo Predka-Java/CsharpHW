108f3a063239ae4bce7b6629b6ba40a1b57d4619
Predka <swaggajugga69@gmail.com>
2022-12-14 05:13:38 +0700

    add 4th task

---
+++ Task4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int res = 0;
+while(res < a) {
+    Console.Write(res + ", ");
+    res = res + 2;
+}
