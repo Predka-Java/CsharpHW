14130e9297fa7ca81d444e88032fba89f9a9930b
Predka <swaggajugga69@gmail.com>
2022-12-14 05:12:30 +0700

    add 3rd task

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+int res = a % 2;
+
+if (res != 0) {
+    Console.WriteLine("Нет");
+} else {
+    Console.WriteLine("Да");
+}
